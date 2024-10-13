b36448bbac74f14ec88271670842704af1cbf29b
Sebastian Correa <43179146+sebastian-correa@users.noreply.github.com>
2024-10-13 19:20:01 -0300

    Initial setup

---
+++ src/chequer/branding.py
@@ -0,0 +1,13 @@
+"""Branding related things like logos for Chequer."""
+
+ascii_brand = """
+ ▗▄▄▖▐▌   ▗▞▀▚▖ ▄▄▄▄ █  ▐▌▗▞▀▚▖ ▄▄▄
+▐▌   ▐▌   ▐▛▀▀▘█   █ ▀▄▄▞▘▐▛▀▀▘█
+▐▌   ▐▛▀▚▖▝▚▄▄▖▀▄▄▄█      ▝▚▄▄▖█
+▝▚▄▄▖▐▌ ▐▌         █
+                   ▀
+"""
+"""A pretty ASCII logo."""
+
+tag_line = "Gestor de descuentos o préstamos."
+"""Brief description of Chequer."""
